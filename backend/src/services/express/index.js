74ed5ca79c10795662c31928810d0b348fb905d3
Blerr511 <lyovghukasyan@gmail.com>
2021-02-03 03:22:20 +0400

    backend render view

--- backend/src/services/express/index.js
+++ backend/src/services/express/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const { body } = require('express-validator');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+
+const path = require('path');
+
 const uploadNewDataController = require('controllers/uploadNewData.controller');
 const app = express();
 
@@ -9,7 +12,10 @@ app.use(cors({ origin: 'localhost:3000' }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/static', express.static('static'));
+app.use(express.static('build'));
 
 app.post('/api/updateData', body('data').isArray(), uploadNewDataController);
-
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../../build', 'index.html'));
+});
 module.exports = app;
