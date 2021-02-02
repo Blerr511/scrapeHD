1776048806bb421fab5cf2f08cf35534f8861ce4
Blerr511 <lyovghukasyan@gmail.com>
2021-02-03 03:56:22 +0400

    app enabled trusty proxy

--- backend/src/services/express/index.js
+++ backend/src/services/express/index.js
@@ -8,12 +8,12 @@ const path = require('path');
 const uploadNewDataController = require('controllers/uploadNewData.controller');
 const app = express();
 
+app.enable('trust proxy');
 app.use(cors({ origin: 'localhost:3000' }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/static', express.static('static'));
 app.use(express.static('build'));
-
 app.post('/api/updateData', body('data').isArray(), uploadNewDataController);
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../../../build', 'index.html'));
